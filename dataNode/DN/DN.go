5d00b50e8aa369e301217e1340b8097214199cab
MiguelCock <macockc@eafit.edu.co>
2024-09-28 11:09:26 -0500

    added other functions

--- dataNode/DN/DN.go
+++ dataNode/DN/DN.go
@@ -48,33 +48,12 @@ func (n *DataNode) DeleteBlock(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 }
 
+// ---------- REPLICATE BLOCK ----------
+func (n *DataNode) ReplicatetBlock(w http.ResponseWriter, r *http.Request) {
+}
+
 // ---------- BLOCK REPORT ----------
 func (n *DataNode) BlockReport() {
-}
-
-// ---------- HEART BEAT ----------
-func (n *DataNode) HeartBeat() {
-	url := "http://localhost:8080"
-
-	data := []byte(`{"status":"alive"`)
-
-	resp, err := http.Post(url, "/heartbeat", bytes.NewBuffer(data))
-	if err != nil {
-		log.Printf("Failed to send heartbeat request: %v", err)
-	}
-	defer resp.Body.Close()
-
-	log.Printf("Response Status: %s\n", resp.Status)
-}
-
-// ---------- CHECK SUM ----------
-func (n *DataNode) CheckSumVerification() {
-}
-
-// ---------- START REST SERVER ----------
-func (n *DataNode) StartRest() {
-	http.HandleFunc("/delete/", n.DeleteBlock)
-
 	sigs := make(chan os.Signal, 1)
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
 
@@ -83,14 +62,94 @@ func (n *DataNode) StartRest() {
 
 	go func() {
 		for range ticker.C {
-			n.HeartBeat()
+
+			url := "http://localhost:8080"
+
+			data := []byte(`{"status":"alive"`)
+
+			resp, err := http.Post(url, "/blockReport", bytes.NewBuffer(data))
+			if err != nil {
+				log.Printf("Failed to send heartbeat request: %v", err)
+			}
+
+			log.Printf("Response Status: %s\n", resp.Status)
+
+			resp.Body.Close()
 		}
 	}()
 
-	log.Println("Server starting on port 8080...")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	<-sigs
+}
+
+// ---------- HEART BEAT ----------
+func (n *DataNode) HeartBeat() {
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	ticker := time.NewTicker(5 * time.Second)
+	defer ticker.Stop()
+
+	go func() {
+		for range ticker.C {
+
+			url := "http://localhost:8080"
+
+			data := []byte(`{"status":"alive"`)
+
+			resp, err := http.Post(url, "/heartbeat", bytes.NewBuffer(data))
+			if err != nil {
+				log.Printf("Failed to send heartbeat request: %v", err)
+			}
+
+			log.Printf("Response Status: %s\n", resp.Status)
+
+			resp.Body.Close()
+		}
+	}()
 
 	<-sigs
+}
+
+// ---------- CHECK SUM ----------
+func (n *DataNode) CheckSumVerification() {
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	ticker := time.NewTicker(5 * time.Second)
+	defer ticker.Stop()
+
+	go func() {
+		for range ticker.C {
+
+			url := "http://localhost:8080"
+
+			data := []byte(`{"status":"alive"`)
+
+			resp, err := http.Post(url, "/checksum", bytes.NewBuffer(data))
+			if err != nil {
+				log.Printf("Failed to send heartbeat request: %v", err)
+			}
+
+			log.Printf("Response Status: %s\n", resp.Status)
+
+			resp.Body.Close()
+		}
+	}()
+
+	<-sigs
+}
+
+// ---------- START REST SERVER ----------
+func (n *DataNode) StartRest() {
+	n.HeartBeat()
+	n.BlockReport()
+	n.CheckSumVerification()
+
+	http.HandleFunc("/deleteBlock/", n.DeleteBlock)
+	http.HandleFunc("/replicatetBlock/", n.ReplicatetBlock)
+
+	log.Println("Server starting on port 8080...")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 // =============== GRPC ==============
@@ -98,7 +157,4 @@ func (n *DataNode) SaveBlock() {
 }
 
 func (n *DataNode) SendReport() {
-}
-
-func (n *DataNode) ReplicatetBeat() {
 }
